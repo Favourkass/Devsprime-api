f3470e4ca032f0de722992e613e1a825c02d15ff
Olatunde Sodiq <olatundesodiq@gmail.com>
2021-06-09 12:48:08 +0100

    remove unecessary files

--- project/db/models/__init__.py
+++ project/db/models/__init__.py
@@ -1,4 +1,3 @@
-from django.db.models.query_utils import subclasses
 from db.models.instructor_course import CourseType, CourseCategory, Course
 from .user import User
 from .instructors import Instructor
