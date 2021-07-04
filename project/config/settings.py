bdbf68041229764589fec16e0a8b4f8cc6d54e38
Olatunde Sodiq <olatundesodiq@gmail.com>
2021-07-04 17:53:34 +0100

    add default to email user and password

--- project/config/settings.py
+++ project/config/settings.py
@@ -157,8 +157,8 @@ SITE_ID = 1
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='dummy@email.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='dummy123')
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
