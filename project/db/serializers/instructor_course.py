3cbee926950f4428b99e438cc11fa1986e5bfa9a
Olatunde Sodiq <olatundesodiq@gmail.com>
2021-07-06 14:13:54 +0100

    use string to represent course type

--- project/db/serializers/instructor_course.py
+++ project/db/serializers/instructor_course.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    type_id = serializers.StringRelatedField()
     class Meta:
         fields = '__all__'
         model = Course
