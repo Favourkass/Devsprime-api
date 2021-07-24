b965f20c1ce086667b694040d4804a6374c19327
Olatunde Sodiq <olatundesodiq@gmail.com>
2021-07-24 14:28:49 +0100

    remove cover img from serializer

--- project/db/serializers/blog_serializer.py
+++ project/db/serializers/blog_serializer.py
@@ -7,7 +7,7 @@ class BlogSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Blog
-        fields = ['id', 'user_id', 'cover_img', 'fullname', 'title', 'short_desc', 'detail', 'created_at', 'updated_at']
+        fields = ['id', 'user_id', 'fullname', 'title', 'short_desc', 'detail', 'created_at', 'updated_at']
 
 class BlogListSerializer(serializers.ModelSerializer):
     fullname = serializers.ReadOnlyField(source='user_id.fullname')
